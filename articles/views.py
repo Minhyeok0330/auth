74db4021f03763e46206bd6b0a58400a404cdc49
minyeok <alsgurgud@naver.com>
2025-03-28 09:48:08 +0900

    modeling, migration

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ArticleForm
 from .models import Article
+from accounts.models import User
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -11,7 +12,7 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
-    
+
 @login_required
 def create(request):
     if request.method=='POST':
@@ -27,3 +28,42 @@ def create(request):
             'form':form,
         }
     return render(request, 'create.html', context)
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    context = {
+        'article' : article
+    }
+
+    return render(request, 'detail.html', context)
+
+def delete(request, id):
+    article = Article.objects.get(id=id) 
+    account = request.user
+    if article.user_id == account.id:
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.id)
+
+def update(request, id):
+    article = Article.objects.get(id=id) 
+    account = request.user
+    
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if article.user_id == account.id:
+            if form.is_valid():
+                form.save()
+                return redirect('articles:detail', article.id)
+
+        else:
+            return redirect('articles:detail', article.id)
+    else:
+        form = ArticleForm(instance=article)
+        context = {
+            'form':form,
+            'article' : article,
+        }
+        return render(request, 'update.html', context)
+        
